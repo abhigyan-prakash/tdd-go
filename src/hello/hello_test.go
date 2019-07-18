3ee12da16cbbbaa301fa8c9ee1a044938b2fda9e
Abhigyan Prakash <abhigyan.prakash@outlook.com>
2019-07-18 18:35:57 +0800

    Integers - Refactor and Source

---
+++ src/hello/hello_test.go
@@ -0,0 +1,37 @@
+package hello
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying tdd-go to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Louies", "French")
+		want := "Bonjour, Louies"
+		assertCorrectMessage(t, got, want)
+	})
+}
