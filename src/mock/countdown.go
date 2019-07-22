0636a12fa296d59fca93a25bad68e8fe06379881
Abhigyan Prakash <abhigyan.prakash@outlook.com>
2019-07-22 11:24:13 +0800

    TDD - Mocks refactored

--- src/mock/countdown.go
+++ src/mock/countdown.go
@@ -10,14 +10,13 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
-type Sleeper interface {
-	Sleep()
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
 }
 
-type DefaultSleeper struct {}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+type Sleeper interface {
+	Sleep()
 }
 
 func Countdown(out io.Writer, sleeper Sleeper)  {
@@ -30,7 +29,11 @@ func Countdown(out io.Writer, sleeper Sleeper)  {
 	fmt.Fprint(out, finalWord)
 }
 
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 func main()  {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
